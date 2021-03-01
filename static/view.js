b21bd3e436755a80897fb9526484c7c417d20aed
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2021-02-28 23:29:47 -0500

    - display ingredients and directions with js instead of jinja syntax

--- static/view.js
+++ static/view.js
@@ -88,4 +88,115 @@ const submit_edit = (new_value)=>{
     }) 
 }
 
+const display_ingredients = (ingredients) =>{
+    const ingredientsCol = document.createElement("div");
+    ingredientsCol.classList.add("col-4", "text-white");
+
+    //Section 1
+    const ingredientsTitleSection = document.createElement("div");
+    const ingredientsTitle = document.createElement("h2");
+    ingredientsTitle.innerHTML = "Ingredients";
+    ingredientsTitle.classList.add("text-center","border-bottom", "pb-2", "text-uppercase");
+    ingredientsTitleSection.classList.add("d-flex");
+
+    ingredientsTitleSection.append(ingredientsTitle);
+
+    //Section 2
+    const ingredientsContainer = document.createElement("div");
+    ingredientsContainer.classList.add("container-fluid","px-0")
+    const unorderedList = document.createElement("ul");
+    unorderedList.classList.add("list-group", "text-dark", "container-fluid","justify-content-between")
+
+    for (let ingredient in ingredients){
+        //INGREDIENT SECTION
+        const listItem = document.createElement("li");
+        const textSectionCol = document.createElement("div");
+        const textSectionRow = document.createElement("div");
+        const inputSection = document.createElement("div");
+        const input = document.createElement("input");
+        const ingredietSection = document.createElement("div");
+        //DELETE SECTION 
+        const deleteCol = document.createElement("div");
+        const deleteRow = document.createElement("div");
+        const deleteSection = document.createElement("div");
+        const deleteButton = document.createElement("span");
+
+
+        deleteButton.innerHTML = "X";
+        deleteButton.classList.add("cursor-pointer");
+        // deleteButton.classList.add("btn", "rounded-3", "btn-outline-light","px-2", "py-0","btn-dark")
+        deleteSection.append(deleteButton);
+        deleteSection.classList.add("col-10", "mx-auto");
+        deleteRow.append(deleteSection);
+        deleteRow.classList.add("row");
+        deleteCol.append(deleteRow);
+        deleteCol.classList.add("col-2");
+
+        ingredietSection.innerHTML = ingredients[ingredient];
+        ingredietSection.classList.add("col-11");
+        input.type = "checkbox";
+        input.value = "";
+        input.classList.add("form-check-input", "me-1");
+        inputSection.append(input);
+        inputSection.classList.add("col-1");
+        textSectionRow.append(inputSection, ingredietSection);
+        textSectionRow.classList.add("row");
+
+        textSectionCol.append(textSectionRow);
+        textSectionCol.classList.add("col-10");
+
+        listItem.append(textSectionCol, deleteCol);
+        listItem.classList.add("row", "text-white","py-1")
+
+        unorderedList.append(listItem);
+
+    }
+    ingredientsContainer.append(unorderedList);
+
+    ingredientsCol.append(ingredientsTitleSection, ingredientsContainer);
+    return ingredientsCol;
+}
+const display_directions = (directions) =>{
+    const directionsCol = document.createElement("div");
+    directionsCol.classList.add("col-8", "text-white");
+
+    //Section 1
+    const directionsTitleSection = document.createElement("div");
+    directionsTitleSection.classList.add("d-flex");
+    const directionsTitle = document.createElement("h2");
+    directionsTitle.innerHTML = "directions";
+    directionsTitle.classList.add("text-center","border-bottom", "pb-2", "text-uppercase");
+    directionsTitleSection.append(directionsTitle);
+
+    //Section 2
+    const directionsContainer = document.createElement("div");
+    directionsContainer.classList.add("container-fluid");
+    const orderedList = document.createElement("ol");
+    orderedList.classList.add("list-group", "text-dark", "container-fluid","justify-content-between", "ps-1")
+    
+    for (let direction in directions){
+        //Direction SECTION
+        const listItem = document.createElement("li");
+        listItem.innerHTML = directions[direction];
+        listItem.classList.add("text-white", "py-1");
+        orderedList.append(listItem);
+    }
+    directionsContainer.append(orderedList);
+
+    directionsCol.append(directionsTitleSection, directionsContainer);
+
+    return directionsCol;
+
+}
+const display_ingredients_and_directions = ()=>{
+    let ingredients = recipe.ingredients;
+    let directions = recipe.directions;
+    const container = document.getElementById("ingredients-and-directions");
+    container.append(display_ingredients(ingredients), display_directions(directions));
+    
+    
+}
+
+display_ingredients_and_directions();
+
 edit.addEventListener("click", display_edit_form)
